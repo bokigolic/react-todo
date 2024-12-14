5b77e7c06ad879760e4cc9e7a71980419a800c25
bokigolic <bokigolic33@gmail.com>
2024-12-13 23:50:23 -0500

    addded some style

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,14 +1,13 @@
 import { useState } from "react";
-import { FaDeleteLeft } from "react-icons/fa6";
+import { FaTrashAlt } from "react-icons/fa"; // Bolja ikonica za brisanje
 
 const TodoApp = () => {
-
   const [task, setTask] = useState("");
   const [todos, setTodos] = useState([]);
 
   const handleInputChange = (e) => {
-    setTask(e.target.value)
-  }
+    setTask(e.target.value);
+  };
 
   const handleAddTask = () => {
     if (task.trim() !== "") {
@@ -16,31 +15,42 @@ const TodoApp = () => {
       setTask("");
     }
   };
+
   const handleDeleteTask = (indexToDelete) => {
-    const updatedTodos = todos.filter((_, index) => index !== indexToDelete)
+    const updatedTodos = todos.filter((_, index) => index !== indexToDelete);
     setTodos(updatedTodos);
-  }
+  };
 
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Add task"
-        value={task}
-        onChange={handleInputChange}
-      />
-      <button onClick={handleAddTask}>Add</button>
-      <ul>
+    <div className="todo-app">
+      <h1 className="title">My To-Do List</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          className="task-input"
+          placeholder="Add a new task"
+          value={task}
+          onChange={handleInputChange}
+        />
+        <button className="add-button" onClick={handleAddTask}>
+          Add
+        </button>
+      </div>
+      <ul className="task-list">
         {todos.map((todo, index) => (
-          <li key={index}>
-            {todo}{" "}
-            <button onClick={() => handleDeleteTask(index)}>{<FaDeleteLeft />}</button>
+          <li key={index} className="task-item">
+            <span>{todo}</span>
+            <button
+              className="delete-button"
+              onClick={() => handleDeleteTask(index)}
+            >
+              <FaTrashAlt />
+            </button>
           </li>
         ))}
-
       </ul>
-
     </div>
-  )
+  );
 };
+
 export default TodoApp;
