cf56fc7888797d5b4f40b52f9e50f5b85d56e755
bokigolic <bokigolic33@gmail.com>
2024-12-13 22:01:13 -0500

    create done

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -3,11 +3,18 @@ import { useState } from "react";
 const TodoApp = () => {
 
   const [task, setTask] = useState("");
-
+  const [todos, setTodos] = useState([]);
 
   const handleInputChange = (e) => {
     setTask(e.target.value)
   }
+
+  const handleAddTask = () => {
+    if (task.trim() !== "") {
+      setTodos([...todos, task]);
+      setTask("");
+    }
+  };
 
   return (
     <div>
@@ -16,8 +23,14 @@ const TodoApp = () => {
         placeholder="Add task"
         value={task}
         onChange={handleInputChange}
-
       />
+      <button onClick={handleAddTask}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+
+      </ul>
 
     </div>
   )
